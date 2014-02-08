8b56b372463b4f49241b358d0fd65c868cd42f4f
Kristine <kristinka89@gmail.com>
2014-02-08 23:15:20 +0200

    Uncorrect instructions

--- Rover.rb
+++ Rover.rb
@@ -1,79 +1,77 @@
-#require 'active_support/core_ext/string'
 require 'active_support/all'
 
 class Rover
   attr_accessor :x_coordinate, :y_coordinate
-
-  def initialize(x, y, l)    
-    @x_coordinate, @y_coordinate = x, y
-    #raise ArgumentError, "Invalid coordinates" unless checkig_coordinate!(@x_coordinate, @y_coordinate)
-	  @start_direction = l
-    #raise ArgumentError, "Invalid direction" unless valid_direction?
-    #Error_message "Invalid direction" unless valid_direction?   
-  #ensure return false
-  end
  
-  DIRECTION = %w[N W S E]
-  ACTION = 'LRM'
-        
+  DIRECTION = %w[N W S E] 
+
   def rover_moving(instructions)
     @look_side = DIRECTION.index(@start_direction)
     instructions.each_char do |i|
       if i == 'L'	    
-        @look_side += 1
-        checking_moving_direction				
-	    elsif i == 'R'	  	
-	      @look_side -= 1
-	      checking_moving_direction			
-	    else moving_forward
-	    end			
+        @look_side += 1       
+     elsif i == 'R'
+       @look_side -= 1 
+     elsif !moving_forward
+        self.output_string = "Uncorrect instructions"
+        break
+      else self.output_string = "#{self.x_coordinate} #{self.y_coordinate} #{self.direction}"
+    end
+      checking_moving_direction
     end   
+  end
+
+  def lands?(x, y, direction)
+     @x_coordinate, @y_coordinate = x, y
+     @start_direction = direction
+     return false  if (!valid_direction? or !valid_coordinate?(@x_coordinate, @y_coordinate))
+     return true
   end  
 
   def direction
-  	DIRECTION[@look_side]
+    DIRECTION[@look_side]
   end
 
   def output_results
-    "#{self.x_coordinate} #{self.y_coordinate} #{self.direction}"
+    output_string
   end
 
   private
 
-    def moving_forward
-      case DIRECTION[@look_side]
-      when  'S'
-        @y_coordinate -= 1 
-        checkig_coordinate!(@x_coordinate, @y_coordinate)
-      when 'E'
-        @x_coordinate += 1
-        checkig_coordinate!(@x_coordinate, @y_coordinate) 
-      when 'N'
-        @y_coordinate += 1
-        checkig_coordinate!(@x_coordinate, @y_coordinate) 
-      else 'W'
-        @x_coordinate -= 1
-        checkig_coordinate!(@x_coordinate, @y_coordinate)
-      end   
-    end
+  attr_accessor :output_string
 
-    def checking_moving_direction
-      if @look_side == DIRECTION.length
-      	@look_side = 0
-      elsif @look_side == -1
-      	@look_side = DIRECTION.length-1
-      end      		
-    end
+  def moving_forward      
+    case DIRECTION[@look_side]
+    when 'S'
+      @y_coordinate -= 1         
+    when 'E'
+      @x_coordinate += 1       
+    when 'N'
+      @y_coordinate += 1        
+    when 'W'
+      @x_coordinate -= 1    
+    end  
+    valid_coordinate?(@x_coordinate, @y_coordinate)
+  end
 
-    def checkig_coordinate!(x, y)
-	    unless x.between?(0, UPPER_X) && y.between?(0, UPPER_Y)
-        return false
-	    end
-    end
+  def checking_moving_direction
+    if @look_side == DIRECTION.length
+      @look_side = 0
+    elsif @look_side == -1
+      @look_side = DIRECTION.length-1
+    end      		
+  end
 
-    def valid_direction?
-      DIRECTION.include?(@start_direction)        
-    end 
+  def valid_coordinate?(x, y)     
+    unless x.between?(0, UPPER_X) && y.between?(0, UPPER_Y)
+      return false        
+    end
+    return true      
+  end
+
+  def valid_direction?
+    DIRECTION.include?(@start_direction)        
+  end
 end
 
 input_data = File.open('input_data.txt', 'r'){ |file| file.read }.split("\n")
@@ -85,10 +83,13 @@ file = File.new('output_data.txt', 'w')
 lines = input_data.length
 1.step(lines,2) do |ln|
   x, y, start_direction = input_data[ln-1].split  
-  rover = Rover.new x.to_i, y.to_i, start_direction
-  rover.rover_moving(input_data[ln])  
-  file.puts rover.output_results
-  file.puts "---------"   
+  rover = Rover.new 
+  if rover.lands?(x.to_i, y.to_i, start_direction)
+    rover.rover_moving(input_data[ln]) 
+    file.puts rover.output_results   
+    file.puts "---------"   
+  else file.puts "Uncorrect initial data"   
+  end   
 end
 file.close
  
